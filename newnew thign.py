7b80124eb12247d11222546fe5f22ea4bb9d4be1
Usir <allanseanalberca1@gmail.com>
2022-07-18 14:42:43 +0800

    (put a newthing)

---
+++ newnew thign.py
@@ -0,0 +1,10 @@
+print("this window is something that is something of a thing taht is to be done that is")
+man = input()
+Feman = []
+if man.lower() == "n":
+    Feman.append("what no")
+elif man.lower() == "y":
+    Feman.append("i did not ader")
+else:
+    Feman.append("stand")
+print(Feman)
